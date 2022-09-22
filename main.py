c0d197d3a0100aeb17627be62251b0b319a53d02
YaritzaP1 <114026269+Yaritza08@users.noreply.github.com>
2022-09-22 18:49:25 +0000

    I add eyes, and separated its legs to be evenly on both sides!

--- main.py
+++ main.py
@@ -10,20 +10,40 @@ spider.pensize(40)
 spider.circle(20)
 
 # Set legs to the number of legs you want on your spider
-legs = 8
+legs = 4
 # Set length to the length you want for the legs
 length = 70
 # Set angle to the direction you want your legs to go out
-angle = 360 / legs
+angle1 = 100 / legs
+angle2 = -100 / legs
 spider.pensize(5)
 # Set count to make it less than the number of legs to activate the loop
 #The loop will run from count until it reaches the number of legs - Choose a count depending on how many loops you want the program to run
 count = 0
 while (count < legs):
   spider.goto(0,20) # Set coordinates to the center of the spider's body
-  spider.setheading(angle*count) #This is the direction of the legs
+  spider.setheading(angle1*count-100) #This is the direction of the legs
   spider.forward(length) # How long it goes forward
   count = count + 1 # Creating increments
 spider.hideturtle() # So the turtle doesn't draw as you go back to the original coordinates
+count=0
+while (count < legs):
+  spider.goto(0,20) # Set coordinates to the center of the spider's body
+  spider.setheading(angle1*count+100) #This is the direction of the legs
+  spider.forward(length) # How long it goes forward
+  count = count + 1 # Creating increments
+spider.hideturtle()
+
+spider.penup()
+spider.goto(-10,0)
+spider.pendown()
+spider.pencolor('white')
+spider.pensize(5)
+# Create a circle!
+spider.circle(2)
+spider.penup()
+spider.goto(-24, 15)
+spider.pendown()
+spider.circle(2)
 wn = trtl.Screen()
 wn.mainloop()
